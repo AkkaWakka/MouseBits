bdec2aca4b07dfd075474f1b0af09e883d5c91f0
Akka-Wakka <AkkaWakka@users.noreply.github.com>
2017-03-21 23:06:59 +0000

    Started designing spinner.

--- gui.lua
+++ gui.lua
@@ -15,6 +15,13 @@ function add_button(player, akkaPlayer)
   })
 end
 
+local function akka_frame(frame_name, frame_direction)
+  return {
+    type = "frame",
+    name = frame_name,
+    direction = frame_direction
+  }
+end
 local function akka_table(table_name, cols)
   return {
     type = "table",
@@ -52,13 +59,11 @@ local function akka_check(check_name, check_state)
     state = check_state
   }
 end
-
-local function gui_box(player_gui)
-  return player_gui.add({
-    type = "frame",
-    name = "akka_box",
-    direction = "vertical"
-  })
+local function akka_text(text_name)
+  return {
+    type = "textfield",
+    name = text_name
+  }
 end
 
 local function gui_header(gui_box)
@@ -73,7 +78,15 @@ local function axis_info_item(identifier, container, akkaPlayer)
   container[identifier].add(akka_label(identifier.."-label", {"controls.akka-"..identifier}))
 end
 
-function pattern_visual(gui_box, akkaPlayer, player)
+function pattern_spinner(gui_box, akkaPlayer)
+  gui_box.add(akka_table("spinner", 2))
+  gui_box.spinner.add(akka_text("value"))
+  gui_box.spinner.add(akka_table("buttons", 1))
+  gui_box.spinner.buttons.add(akka_button("akka-spinner-up", "akka_spinner_up"))
+  gui_box.spinner.buttons.add(akka_button("akka-spinner-down", "akka_spinner_down"))
+end
+
+function pattern_visual(gui_box, akkaPlayer)
   local pattern = gui_box.add(akka_table("pattern", akkaPlayer.patternSlots))
   for i = 1, akkaPlayer.patternSlots, 1 do
     pattern.add(akka_check("akka-pattern-check-"..i, not not akkaPlayer.pattern[i]))
@@ -85,6 +98,7 @@ local function pattern_gui(gui_box, akkaPlayer)
   local on_off_caption
   if akkaPlayer.patternState then
     on_off_caption = {"gui.akka-on"}
+    pattern_spinner(gui_box, akkaPlayer)
     pattern_visual(gui_box, akkaPlayer)
   else
     on_off_caption = {"gui.akka-off"}
@@ -95,7 +109,7 @@ end
 
 function add_box(player, akkaPlayer)
   destroy_gui(akkaPlayer)
-  akkaPlayer.gui = gui_box(player.gui.top)
+  akkaPlayer.gui = player.gui.top.add(akka_frame("akka_box", "vertical"))
   local box = akkaPlayer.gui
   gui_header(box)
   axis_info_item("onlyX", box, akkaPlayer)
--- prototypes/styles.lua
+++ prototypes/styles.lua
@@ -1,18 +1,33 @@
 local default = data.raw["gui-style"]["default"]
 
-local function make_g_set(xpos, ypos)
+local function make_g_set(xpos, ypos, wwidth, hheight)
   return {
     type = "monolith",
+    top_monolith_border = 0,
+    right_monolith_border = 0,
+    bottom_monolith_border = 0,
+    left_monolith_border = 0,
     monolith_image = {
       filename = "__MouseBits__/graphics/gui.png",
       priority = "extra-high-no-scale",
-      width = 24,
-      height = 24,
+      width = wwidth,
+      height = hheight,
       x = xpos,
       y = ypos
     }
   }
 end
+
+default.akka_button = {
+  type = "button_style",
+  parent = "button_style",
+  width = 24,
+  height = 24,
+  top_padding = 0,
+  right_padding = 0,
+  bottom_padding = 0,
+  left_padding = 0
+}
 
 default.akka_pointer = {
   type = "button_style",
@@ -23,9 +38,9 @@ default.akka_pointer = {
   right_padding = 8,
   bottom_padding = 8,
   left_padding = 8,
-  default_graphical_set = make_g_set(0, 0),
-  hovered_graphical_set = make_g_set(0, 25),
-  clicked_graphical_set = make_g_set(0, 49)
+  default_graphical_set = make_g_set(0, 0, 24, 24),
+  hovered_graphical_set = make_g_set(0, 25, 24, 24),
+  clicked_graphical_set = make_g_set(0, 50, 24, 24)
 }
 
 default.akka_close = {
@@ -37,9 +52,37 @@ default.akka_close = {
   right_padding = 8,
   bottom_padding = 8,
   left_padding = 8,
-  default_graphical_set = make_g_set(25, 0),
-  hovered_graphical_set = make_g_set(25, 25),
-  clicked_graphical_set = make_g_set(25, 49)
+  default_graphical_set = make_g_set(25, 0, 24, 24),
+  hovered_graphical_set = make_g_set(25, 25, 24, 24),
+  clicked_graphical_set = make_g_set(25, 50, 24, 24)
+}
+
+default.akka_spinner_up = {
+  type = "button_style",
+  parent = "button_style",
+  width = 12,
+  height = 9,
+  top_padding = 0,
+  right_padding = 0,
+  bottom_padding = 0,
+  left_padding = 0,
+  default_graphical_set = make_g_set(50, 0, 12, 9),
+  hovered_graphical_set = make_g_set(50, 25, 12, 9),
+  clicked_graphical_set = make_g_set(50, 50, 12, 9)
+}
+
+default.akka_spinner_down = {
+  type = "button_style",
+  parent = "button_style",
+  width = 12,
+  height = 9,
+  top_padding = 0,
+  right_padding = 0,
+  bottom_padding = 0,
+  left_padding = 0,
+  default_graphical_set = make_g_set(50, 10, 12, 9),
+  hovered_graphical_set = make_g_set(50, 35, 12, 9),
+  clicked_graphical_set = make_g_set(50, 60, 12, 9)
 }
 
 default.akka_table = {
